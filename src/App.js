78cc7a95d93e22500b21f0cd19f9b96836bc91a0
Md. Tahmid Chowdhury <mdtahmidchowdhury12@gmail.com>
2022-11-05 12:53:14 +0600

    Store Added and New Component added

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import './App.css';
 import { Provider } from 'react-redux';
 import store from './redux/store';
 import CakeContainer from './components/CakeContainer';
+import IceCreamContainer from './components/IceCreamContainer';
 
 function App() {
   return (
     <Provider store={store}>
       <div className='App'>
         <CakeContainer/>
+        <IceCreamContainer />
       </div>
     </Provider>
   );
---
+++ src/redux/icecream/iceCreamReducer.js
@@ -0,0 +1,18 @@
+import { BUY_ICE_CREAM } from "./iceCreamTypes";
+
+const initialState = {
+    numberOfIceCreams: 10
+}
+
+const iceCreamReducer= (state=initialState, action) => {
+    switch(action.type){
+        case BUY_ICE_CREAM:
+            return{
+                ...state,
+                numberOfIceCreams: state.numberOfIceCreams - 1
+            }
+        default: return state;
+    }
+}
+
+export default iceCreamReducer;
